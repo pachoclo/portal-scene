12357bbfb9bea504a4ebaee1866cc375646d2e2b
Francisco Jaramillo <pacho@live.ca>
2023-01-23 16:04:49 -0500

    add fireflies

--- src/shaders/fireflies/fragment.glsl
+++ src/shaders/fireflies/fragment.glsl
@@ -4,5 +4,5 @@ void main() {
   float distanceToCenter = distance(gl_PointCoord, vec2(0.5));
   float strength = (0.05 / distanceToCenter) - 0.05 * 2.0;
 
-  gl_FragColor = vec4(0.7, 0.7, 0.0, strength  * (sin(uTime) + 1.5));
+  gl_FragColor = vec4(0.7, 0.7, 0.0, strength  * (cos(uTime) + 1.7));
 }
